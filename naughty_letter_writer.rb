c64cf8c86c58e879aa0d6409a0294c097c4f0762
Michelle Cinello <m.cinello@gmail.com>
2017-10-03 20:03:39 -0400

    made naughty letters with naughty_letter_writer.rb based on the naughty_letter_template.txt.rb

---
+++ naughty_letter_writer.rb
@@ -0,0 +1,26 @@
+require 'erb'
+
+kids_data   = File.read('data/kids-data.txt')
+naughty_letter = File.read('templates/naughty_letter_template.txt.erb')
+
+kids_data.each_line do |kid|
+
+  kid_data_array = kid.split
+
+  name     = kid_data_array[0]
+  gender   = kid_data_array[1]
+  behavior = kid_data_array[2]
+  toys     = kid_data_array[3..8]
+  infraction = kid_data_array[15..24]
+
+  next unless behavior == 'naughty'
+
+  filename    = 'letters/naughty/' + name + '.txt'
+  letter_text = ERB.new(naughty_letter, nil, '-').result(binding)
+
+  puts "Writing #{filename}."
+  File.write(filename, letter_text)
+
+end
+
+puts 'Done!'
